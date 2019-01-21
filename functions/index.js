445708a9e364822594f8a4a692ed4da9855c4f24
sd1998 <shashvat51@gmail.com>
2019-01-21 19:25:08 +0530

    Initial commit

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,13 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express')
+const bodyParser = require('body-parser');
+const async = require('asyncawait/async');
+const await = require('asyncawait/await');
+const app = express();
+admin.initializeApp();
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
