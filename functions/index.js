5dcd6ef529d029bac3ec074dd00c2256d15bed5e
sd1998 <shashvat51@gmail.com>
2019-01-22 13:27:52 +0530

    Routes added

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,38 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const async = require('asyncawait/async');
 const await = require('asyncawait/await');
+const httpErrors = require('http-errors');
+const config = require('./config.js');
 const app = express();
+
 admin.initializeApp();
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cors())
 
+app.use(function(req,res,next){
+  if(req.body.api_key == config.API_KEY){
+    next()
+  }
+  else{
+    next(httpErrors(403,"Forbidden"))
+  }
+})
+
+app.get("/sendnotif/register",function(req,res){
+  
+})
+
+app.get("/sendnotif/event",function(req,res){
+
+})
+
+app.use(function(req,res,next){
+  next(httpErrors(404,"Page not found"))
+})
+
+module.exports.api = functions.https.onRequest(app);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
