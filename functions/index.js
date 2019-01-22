b69c07797fab184ddf27386afa646fd0991880d7
sd1998 <shashvat51@gmail.com>
2019-01-22 13:48:15 +0530

    Cleanup function added

--- functions/index.js
+++ functions/index.js
@@ -14,6 +14,34 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(cors())
 
+function getTokens(snapshot){
+  if(snapshot.exists()){
+    const data = snapshot.val()
+    const keys = Object.keys(data)
+    const tokens = []
+    for(var i=0;i < len(keys);i++){
+      tokens.push(data[keys[i]])
+    }
+    return tokens
+  }
+  return null
+}
+
+function cleanup(response,tokens,keys){
+  const tokensToRemove = {}
+  response.results.forEach(function(result,index){
+    const error = result.error
+    if(error){
+      console.error('Failure sending notification to ',tokens[index],error)
+      if(error.code == 'messaging/invalid-registration-token' || error.code == 'messaging/registration-token-not-registered'){
+        tokensToRemove[`/data/fcm/tokens/${keys[index]}`] = null
+        tokensToRemove[`/data/fcm/user/${keys[index]}`] = null
+      }
+    }
+  })
+  return admin.database().ref().update(tokensToRemove)
+}
+
 app.use(function(req,res,next){
   if(req.body.api_key == config.API_KEY){
     next()
@@ -30,6 +58,30 @@ app.get("/sendnotif/register",function(req,res){
 app.get("/sendnotif/event",function(req,res){
 
 })
+
+app.get("/sendnotif/custom",async(function(req,res,next){
+  const payload = {
+    data: {
+      "heading": req.body.headingt,
+      "content": req.body.content
+    }
+  }
+  const allTokens = await(admin.database().ref("/data/fcm/tokens"))
+  const tokens = getTokens(allTokens)
+  if(tokens != null){ 
+    const response = await(admin.messaging().sendToDevice(tokens,payload))  
+    await(cleanup(response,tokens,Object.keys(allTokens.val())))
+    res.status(203).json({
+      "response": "Notification sent"
+    })
+  }
+  else{
+    res.status(500).json({
+      "response": "No tokens present"
+    })
+  }
+  
+}))
 
 app.use(function(req,res,next){
   next(httpErrors(404,"Page not found"))
