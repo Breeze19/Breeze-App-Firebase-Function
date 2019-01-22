96e2cf96a787f1c7e55523ba6d99ec2b88f7e38d
sd1998 <shashvat51@gmail.com>
2019-01-22 16:41:55 +0530

    Shifting to different project

--- functions/index.js
+++ functions/index.js
@@ -89,9 +89,3 @@ app.use(function(req,res,next){
 })
 
 module.exports.api = functions.https.onRequest(app);
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
